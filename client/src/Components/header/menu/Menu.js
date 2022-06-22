fc00bc5c0e758a398b08a447fedcc0b79c602698
rkshaown <rkshawon36@gamil.com>
2022-06-22 12:20:35 +0600

    ok

--- client/src/Components/header/menu/Menu.js
+++ client/src/Components/header/menu/Menu.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { Context } from '../../../context/shooping/Context'
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import {RiAdminLine} from 'react-icons/ri'
 import {FiUser} from 'react-icons/fi'
 import {MdOutlineClose} from 'react-icons/md'
@@ -20,14 +20,14 @@ function Menu({open, close}) {
     <div className='menu_container'>
     <div className="menu_list">
     <div className="menu_amazon_title">
-      <NavLink to="/login" onClick={logOut} style={{ color: 'inherit',  textDecoration: 'inherit'}}>
+      <a href="/login" onClick={logOut} style={{ color: 'inherit',  textDecoration: 'inherit'}}>
       <div className="menu_navbar">
           <FiUser className='menu_navbar_profile_icon'/>
           <div className="menu_navbar_text">
               <h5>{user? `Hello, ${user.name}`: `Welcome to Amazon`}</h5>
           </div>
       </div>
-    </NavLink> 
+    </a> 
 
       <small>Browse</small>
       <div>Amazon</div>
@@ -46,27 +46,27 @@ function Menu({open, close}) {
     </NavLink>
     {
         user?.isAdmin &&
-        <NavLink to="/admin" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+        <a href="/admin" style={{ color: 'inherit', textDecoration: 'inherit'}}>
         <div className="menu_navbars">
             <RiAdminLine className='menu_navbar_icon'/>
             <h5 className="menu_navbar_text">Admin</h5>
         </div>
-    </NavLink>
+    </a>
 } 
 {
   user ?
-  <NavLink to="/login" onClick={logOut} style={{ color: 'inherit', textDecoration: 'inherit'}}>
+  <a href="/login" onClick={logOut} style={{ color: 'inherit', textDecoration: 'inherit'}}>
   <div className="menu_navbars">
       <BiLogOut className='menu_navbar_icon'/>
       <h5 className="menu_navbar_text">Sing Out</h5>
   </div>
-</NavLink> :
-<Link to="/login" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+</a> :
+<a href="/login" style={{ color: 'inherit', textDecoration: 'inherit'}}>
 <div className="menu_navbars">
   <BiLogOut className='menu_navbar_icon'/>
   <h5 className="menu_navbar_text">Sing in</h5>
 </div>
-</Link>
+</a>
 }
 </div>  
   <div onClick={close}>
