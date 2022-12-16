e5a3cdb24c62a056b7a12ed7ca3d44e284b9d50b
rkshaown <rkshawon36@gamil.com>
2022-12-17 00:10:45 +0600

    updated

--- client/src/Components/header/Navbar.js
+++ client/src/Components/header/Navbar.js
@@ -1,122 +1,144 @@
-import './navbar.css'
+import "./navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
-import {FiSearch} from 'react-icons/fi'
-import {FiShoppingCart} from 'react-icons/fi'
-import {RiAdminLine} from 'react-icons/ri'
-import {FiUser} from 'react-icons/fi'
-import {FiMenu} from 'react-icons/fi'
-import {BiHistory} from 'react-icons/bi'
-import amazonLogo from './amazonlogo.png'
-import { Context } from '../../context/shooping/Context';
-import { useContext, useEffect, useRef, useState } from 'react';
-import { BasketContext } from '../../context/shooping/BasketContext';
-import Menu from './menu/Menu';
+import { FiSearch } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
+import { RiAdminLine } from "react-icons/ri";
+import { FiUser } from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
+import { BiHistory } from "react-icons/bi";
+import amazonLogo from "./amazonlogo.png";
+import { Context } from "../../context/shooping/Context";
+import { useContext, useEffect, useRef, useState } from "react";
+import { BasketContext } from "../../context/shooping/BasketContext";
+import Menu from "./menu/Menu";
 
-function Navbar({scroll, scrollController}) {
-    const {user, dispatch} = useContext(Context)
-    const {basket} = useContext(BasketContext)
-    const [searchText, setSearchtext] = useState('')
-    const [openMenu, setOpenMenu] = useState(false)
-    const navigate = useNavigate()
-    const scrlRef = useRef()
+function Navbar({ scroll, scrollController }) {
+  const { user, dispatch } = useContext(Context);
+  const { basket } = useContext(BasketContext);
+  const [searchText, setSearchtext] = useState("");
+  const [openMenu, setOpenMenu] = useState(false);
+  const navigate = useNavigate();
+  const scrlRef = useRef();
 
-    const logOut = ()=>{
-        dispatch({type: "LOGOUT"})
-      }
-      const search = (e) => {
-        if (e.key === "Enter") {
-            searchText && navigate(`/search?name=${searchText}`)
-        }
+  const logOut = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+  const search = (e) => {
+    if (e.key === "Enter") {
+      searchText && navigate(`/search?name=${searchText}`);
     }
-    useEffect(()=>{
-        scroll(scrlRef)
-        scrollController(openMenu)
-      },[openMenu])
+  };
+  useEffect(() => {
+    scroll(scrlRef);
+    scrollController(openMenu);
+  }, [openMenu]);
   return (
-      <header className='headerContainer' ref = {scrlRef}>
-      {
-        openMenu && <Menu 
-        open = {openMenu}
-        close = {()=>setOpenMenu(false)}/>
-    }
-        <div className = "leftheader">
-        
-            <div className="logo_container">
-                <div onClick={()=>setOpenMenu(true)}  className="menu">
-                <FiMenu className='menuicon'/>
-            </div>
-            <NavLink  to="/" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <img src={amazonLogo} alt="amazon" />
-            </NavLink>
-            </div>    
-       
-         
-            <div className="search">
-                <input type="text" onChange={(e)=>setSearchtext(e.target.value)} onKeyPress={search}/>
-                <NavLink to={searchText && `/search?name=${searchText}`}
-                className='searchicon'
-                style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                    <FiSearch/>
-                </NavLink>
-            </div>
+    <header className="headerContainer" ref={scrlRef}>
+      {openMenu && <Menu open={openMenu} close={() => setOpenMenu(false)} />}
+      <div className="leftheader">
+        <div className="logo_container">
+          <div onClick={() => setOpenMenu(true)} className="menu">
+            <FiMenu className="menuicon" />
+          </div>
+          <NavLink
+            to="/"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <img src={amazonLogo} alt="amazon" />
+          </NavLink>
         </div>
 
-        <div className="middleheader">
-            <NavLink to="/cart" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <div className="shoopingCard">
-                    <div className='card_container'>
-                        <FiShoppingCart className='cardicon'/>
-                        <span>{basket?.length}</span>
-                    </div> 
-                    <h5 className="card">Card</h5> 
-                </div>
-            </NavLink>
+        <div className="search">
+          <input
+            type="text"
+            onChange={(e) => setSearchtext(e.target.value)}
+            onKeyPress={search}
+          />
+          <NavLink
+            to={searchText && `/search?name=${searchText}`}
+            className="searchicon"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <FiSearch />
+          </NavLink>
         </div>
-        <div className="rigthheader ">
-            {
-                user ? <NavLink to="/login" onClick={logOut} style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <div className="navbars">
-                    <FiUser className='navbar_icon'/>
-                    <div className="navbar_text">
-                        <h5>{`Hi, ${user.name}`}</h5>
-                        <h5 className='singout'>Sing out</h5>
-                    </div>
-                </div>
-            </NavLink> :
-            <NavLink to="/login" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-            <div className="navbars">
-                <FiUser className='navbar_icon'/>
-                <h5 className="navbar_text">Sing in</h5>
+      </div>
+
+      <div className="middleheader">
+        <NavLink
+          to="/cart"
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
+          <div className="shoopingCard">
+            <div className="card_container">
+              <FiShoppingCart className="cardicon" />
+              <span>{basket?.length}</span>
             </div>
-            </NavLink>
-        }
-            <NavLink to="/orderhistory" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <div className='navbars'>
-                    <BiHistory className='navbar_icon'/>
-                    <h5 className="navbar_text">Orders</h5> 
-                </div>
-            </NavLink>
-            <NavLink to="/cart" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <div className="shoopingCard">
-                    <div className='card_container'>
-                        <FiShoppingCart className='cardicon'/>
-                        <span>{basket?.length}</span>
-                    </div> 
-                    <h5 className="card navbar_text">Card</h5> 
-                </div>
-            </NavLink>
-            {
-                user?.isAdmin &&
-                <NavLink to="/admin" style={{ color: 'inherit',  textDecoration: 'inherit'}}>
-                <div className="navbars">
-                    <RiAdminLine className='navbar_icon'/>
-                    <h5 className="navbar_text">Admin</h5>
-                </div>
-            </NavLink>
-        } 
-        </div>
-      </header>
-  )
+            <h5 className="card">Card</h5>
+          </div>
+        </NavLink>
+      </div>
+      <div className="rigthheader ">
+        {user ? (
+          <NavLink
+            to="/login"
+            onClick={logOut}
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="navbars">
+              <FiUser className="navbar_icon" />
+              <div className="navbar_text">
+                <h5>{`Hi, ${user.name}`}</h5>
+                <h5 className="singout">Sign out</h5>
+              </div>
+            </div>
+          </NavLink>
+        ) : (
+          <NavLink
+            to="/login"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="navbars">
+              <FiUser className="navbar_icon" />
+              <h5 className="navbar_text">Sign in</h5>
+            </div>
+          </NavLink>
+        )}
+        <NavLink
+          to={user ? "/orderhistory" : "/login"}
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
+          <div className="navbars">
+            <BiHistory className="navbar_icon" />
+            <h5 className="navbar_text">Orders</h5>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/cart"
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
+          <div className="shoopingCard">
+            <div className="card_container">
+              <FiShoppingCart className="cardicon" />
+              <span>{basket?.length}</span>
+            </div>
+            <h5 className="card navbar_text">Card</h5>
+          </div>
+        </NavLink>
+        {user?.isAdmin && (
+          <NavLink
+            to="/admin"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <div className="navbars">
+              <RiAdminLine className="navbar_icon" />
+              <h5 className="navbar_text">Admin</h5>
+            </div>
+          </NavLink>
+        )}
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,208 +1,243 @@
-import './register.css'
-import axios from 'axios';
-import React, { useRef, useState } from 'react'
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import amazoneLogo from './Amazon logo.png'
+import "./register.css";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import amazoneLogo from "./Amazon logo.png";
 
 function Register() {
   const name = useRef();
   const email = useRef();
   const password = useRef();
   const confirmPassword = useRef();
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false);
   let navigate = useNavigate();
-  const [errorMessageName, setErrorMessageName] = useState('')
-  const [errorMessageEmail, setErrorMessageEmail] = useState('')
-  const [errorMessagePass, setErrorMessagePass] = useState('')
-  const [errorMessagePass2, setErrorMessagePass2] = useState('')
-  const [serverError, setServerError] = useState('')
-  let ename, eemail, epass, epass2 = true
+  const [errorMessageName, setErrorMessageName] = useState("");
+  const [errorMessageEmail, setErrorMessageEmail] = useState("");
+  const [errorMessagePass, setErrorMessagePass] = useState("");
+  const [errorMessagePass2, setErrorMessagePass2] = useState("");
+  const [serverError, setServerError] = useState("");
+  let ename,
+    eemail,
+    epass,
+    epass2 = true;
 
-  const nameCheck = ()=>{
-	let spaceCheck = name.current.value
-    const validateName=()=>{
-      if(String(spaceCheck.split(/\s/).join('')).match(/^[a-zA-Z\-]+$/))
-        return true
-      else
-        return false
-    }
-
-    if(!validateName()){
-      ename = true
-      setErrorMessageName("Only Characters A-Z, a-z and '-' are  acceptable.")
-    }
-    else{
-      if(name.current.value.length > 15){
-        ename = true
-        setErrorMessageName("Username must be less than 15 characters")
-      }
-      else{
-        ename = false
-        setErrorMessageName("")
-      }
-    }
-
-  }
-  const emailCheck = ()=>{
-    const validateEmail = () => {
-      if(String(email.current.value).match(
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      ))
-        return true
-      else
-        return false
+  const nameCheck = () => {
+    let spaceCheck = name.current.value;
+    const validateName = () => {
+      if (String(spaceCheck.split(/\s/).join("")).match(/^[a-zA-Z\-]+$/))
+        return true;
+      else return false;
     };
-   
-    if(!validateEmail()){
-      eemail = true
-      setErrorMessageEmail("Email is not valid")
+
+    if (!validateName()) {
+      ename = true;
+      setErrorMessageName("Only Characters A-Z, a-z and '-' are  acceptable.");
+    } else {
+      if (name.current.value.length > 15) {
+        ename = true;
+        setErrorMessageName("Username must be less than 15 characters");
+      } else {
+        ename = false;
+        setErrorMessageName("");
+      }
     }
-    else{
-      eemail = false
-      setErrorMessageEmail("")
-    }
-  }
-  const passCheck = ()=>{
-    if(password.current.value?.length <6){
-      epass = true
-      setErrorMessagePass("Password can not be less than 6 character")
-    }
-    else{
-      epass = false
-      setErrorMessagePass("")
-    }
-  }
-  const pass2Check = ()=>{
-    if(password.current.value === confirmPassword.current.value){
-      epass2 = false
-      setErrorMessagePass2("")
-    }
-    else{
-      epass2 = true
-      setErrorMessagePass2("Password does not match")
-    }
-  }
-  const handleChange = (e) => {
-    setIsAdmin( e.target.checked )
   };
-  const formValidation = ()=>{
-    if(name.current.value ===''){
-      ename = true
-      setErrorMessageName("Name can not be empty")
-    }
-    else{
-      nameCheck()
-    }
-    
-  
-    if(email.current.value ===''){
-      eemail = true
-      setErrorMessageEmail("Email can not be empty")
-    }
-    else{
-      emailCheck()
-    }
-      
-    
-    if(password.current.value ===''){
-      epass = true
-      setErrorMessagePass("Password can not be empty")
-    }
-    else{
-      passCheck()
-    }
-    
-    if(confirmPassword.current.value ===''){
-      epass2  =true
-      setErrorMessagePass2("Confirm Password can not be empty")
-    }
-    else{
-      pass2Check()
-    }
-  }
+  const emailCheck = () => {
+    const validateEmail = () => {
+      if (
+        String(email.current.value).match(
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+      )
+        return true;
+      else return false;
+    };
 
+    if (!validateEmail()) {
+      eemail = true;
+      setErrorMessageEmail("Email is not valid");
+    } else {
+      eemail = false;
+      setErrorMessageEmail("");
+    }
+  };
+  const passCheck = () => {
+    if (password.current.value?.length < 6) {
+      epass = true;
+      setErrorMessagePass("Password can not be less than 6 character");
+    } else {
+      epass = false;
+      setErrorMessagePass("");
+    }
+  };
+  const pass2Check = () => {
+    if (password.current.value === confirmPassword.current.value) {
+      epass2 = false;
+      setErrorMessagePass2("");
+    } else {
+      epass2 = true;
+      setErrorMessagePass2("Password does not match");
+    }
+  };
+  const handleChange = (e) => {
+    setIsAdmin(e.target.checked);
+  };
+  const formValidation = () => {
+    if (name.current.value === "") {
+      ename = true;
+      setErrorMessageName("Name can not be empty");
+    } else {
+      nameCheck();
+    }
 
-  const errorContainer = ()=>{
-    if(ename || eemail || epass || epass2)
-      return true
-    else
-      return false
-  }
+    if (email.current.value === "") {
+      eemail = true;
+      setErrorMessageEmail("Email can not be empty");
+    } else {
+      emailCheck();
+    }
 
-  const handleRegisterClick = async  (e)=>{
-    e.preventDefault()
+    if (password.current.value === "") {
+      epass = true;
+      setErrorMessagePass("Password can not be empty");
+    } else {
+      passCheck();
+    }
+
+    if (confirmPassword.current.value === "") {
+      epass2 = true;
+      setErrorMessagePass2("Confirm Password can not be empty");
+    } else {
+      pass2Check();
+    }
+  };
+
+  const errorContainer = () => {
+    if (ename || eemail || epass || epass2) return true;
+    else return false;
+  };
+
+  const handleRegisterClick = async (e) => {
+    e.preventDefault();
     const userInfo = {
       name: name.current.value,
       email: email.current.value,
       password: password.current.value,
-      isAdmin: isAdmin
-    }
-    formValidation()
-    if(!errorContainer()){
-      try{
-        const reg = await axios.post('/auth/register', userInfo)
-        await axios.post("/order/orders",{ user_id: reg.data._id })
-        reg && navigate('/login')
-      }catch(err){
+      isAdmin: isAdmin,
+    };
+    formValidation();
+    if (!errorContainer()) {
+      try {
+        const reg = await axios.post("/auth/register", userInfo);
+        await axios.post("/order/orders", { user_id: reg.data._id });
+        reg && navigate("/login");
+      } catch (err) {
         setServerError("Something went wrong");
       }
-
     }
-  }
+  };
 
   return (
-    <div className='sing_up_container'>
-    <NavLink  to="/"
-      style={{ color: 'inherit',  textDecoration: 'inherit'}} className="amazon_logo">
-      <img src={amazoneLogo} alt='amazon logo' />
-    </NavLink>
+    <div className="sing_up_container">
+      <NavLink
+        to="/"
+        style={{ color: "inherit", textDecoration: "inherit" }}
+        className="amazon_logo"
+      >
+        <img src={amazoneLogo} alt="amazon logo" />
+      </NavLink>
       <div className="registration_login_container">
-      <div className="sing_up">
-        <h2>Create Account</h2>
-        <div className="singup_name">
-          <label htmlFor="name">Your name</label>
-          <input type="text" id="name" className="name_field" ref = {name}
-          onClick= {()=>{setErrorMessageName(''); setServerError('')}}/>
-          <small >{errorMessageName}</small>
+        <div className="sing_up">
+          <h2>Create Account</h2>
+          <div className="singup_name">
+            <label htmlFor="name">Your name</label>
+            <input
+              type="text"
+              id="name"
+              className="name_field"
+              ref={name}
+              onClick={() => {
+                setErrorMessageName("");
+                setServerError("");
+              }}
+            />
+            <small>{errorMessageName}</small>
+          </div>
+          <div className="singup_email">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              className="email_field"
+              ref={email}
+              onClick={() => {
+                setErrorMessageEmail("");
+                setServerError("");
+              }}
+            />
+            <small>{errorMessageEmail}</small>
+          </div>
+          <div className="singup_password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              placeholder="At least 6 characters"
+              className="password_field"
+              ref={password}
+              onClick={() => {
+                setErrorMessagePass("");
+                setServerError("");
+              }}
+            />
+            <small>{errorMessagePass}</small>
+          </div>
+          <div className="singup_confirm_password">
+            <label htmlFor="confirm_password">Re-enter password</label>
+            <input
+              type="text"
+              id="confirm_password"
+              className="confirm_password_field"
+              ref={confirmPassword}
+              onClick={() => {
+                setErrorMessagePass2("");
+                setServerError("");
+              }}
+            />
+            <small>{errorMessagePass2}</small>
+          </div>
+          <div className="isAdmin">
+            <input
+              type="checkbox"
+              id="checkBox"
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="checkBox">Admin</label>
+          </div>
+
+          <div className="sing_up_btn" onClick={handleRegisterClick}>
+            <button>Continue</button>
+          </div>
+          <span>
+            By createing an account, you agree to Amazon's
+            <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088">
+              {" "}
+              Condition of Use{" "}
+            </a>
+            and
+            <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&nodeId=468496">
+              {" "}
+              Privacy Notic{" "}
+            </a>
+          </span>
+          <p>
+            Already have an account? <Link to="/login">Sign-In</Link>
+          </p>
+          <div>{serverError}</div>
         </div>
-        <div className="singup_email">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" className="email_field" ref = {email}
-          onClick= {()=>{setErrorMessageEmail(''); setServerError('')}}/>
-          <small >{errorMessageEmail}</small>
-        </div>
-        <div className="singup_password">
-          <label htmlFor="password">Password</label>
-          <input type="text" id="password" placeholder='At least 6 characters'
-            className="password_field" ref = {password}
-            onClick= {()=>{setErrorMessagePass(''); setServerError('')}}/>
-          <small >{errorMessagePass}</small>
-        </div>
-        <div className="singup_confirm_password">
-          <label htmlFor="confirm_password">Re-enter password</label>
-          <input type="text" id="confirm_password"
-            className="confirm_password_field" ref = {confirmPassword}
-            onClick= {()=>{setErrorMessagePass2(''); setServerError('')}}/>
-          <small >{errorMessagePass2}</small>
-        </div>
-        <div className="isAdmin">
-          <input type="checkbox" id='checkBox' onChange={handleChange}></input>
-          <label htmlFor="checkBox">Admin</label>
-        </div>
-        
-        <div className="sing_up_btn"  onClick={handleRegisterClick}><button>Continue</button></div>
-        <span>
-          By createing an account, you agree to Amazon's
-          <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088"> Condition of Use </a>
-           and
-          <a href="https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&nodeId=468496"> Privacy Notic </a>
-        </span>
-        <p>Already have an account? <Link to="/login">Sing-In</Link></p>
-        <div>{serverError}</div>
-      </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
