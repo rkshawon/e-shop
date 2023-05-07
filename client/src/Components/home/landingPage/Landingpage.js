2e98bdaf1f4652230b0b68298e42729ce88268d8
rkshaown <rkshawon36@gamil.com>
2023-05-07 22:17:23 +0600

    updated

--- client/src/Components/home/landingPage/Landingpage.js
+++ client/src/Components/home/landingPage/Landingpage.js
@@ -1,139 +1,145 @@
-import './landingpage.css'
-import i1 from './BackgroundImages/image1.jpg'
-import i2 from './BackgroundImages/image2.jpg'
-import i3 from './BackgroundImages/image3.jpg'
-import i4 from './BackgroundImages/image4.jpg'
-import i5 from './BackgroundImages/image5.jpg'
-import { useEffect, useRef, useState } from 'react'
-import {AiOutlineLeft} from 'react-icons/ai'
-import {AiOutlineRight} from 'react-icons/ai'
-import phone from './Titleimage/phone.jpg'
-import computers from './Titleimage/computers.jpg'
-import laptops from './Titleimage/laptops.jpg'
+import "./landingpage.css";
+import i1 from "./BackgroundImages/image1.jpg";
+import i2 from "./BackgroundImages/image2.jpg";
+import i3 from "./BackgroundImages/image3.jpg";
+import i4 from "./BackgroundImages/image4.jpg";
+import i5 from "./BackgroundImages/image5.jpg";
+import { useEffect, useRef, useState } from "react";
+import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
+import phone from "./Titleimage/phone.jpg";
+import computers from "./Titleimage/computers.jpg";
+import laptops from "./Titleimage/laptops.jpg";
 
-function Landingpage({scrlF, slidescrlF, destopScrlF}) {
-    const [backImgControllerLeft, setBackImgControllerLeft] = useState(4)
-    const [backImgControllerRight, setBackImgControllerRight] = useState(1)
-    const [imageToRender, setImageToRender] = useState(0)
-    const imageRef = useRef()
+function Landingpage({ scrlF, slidescrlF, destopScrlF }) {
+  const [backImgControllerLeft, setBackImgControllerLeft] = useState(4);
+  const [backImgControllerRight, setBackImgControllerRight] = useState(1);
+  const [imageToRender, setImageToRender] = useState(0);
+  const imageRef = useRef();
 
-    const homeImage = [
-        {
-            id : 1,
-            isrc : i1
-        },
-        {
-            id : 2,
-            isrc : i2
-        },
-        {
-            id : 3,
-            isrc : i3
-        },
-        {
-            id : 4,
-            isrc : i4
-        },
-        {
-            id : 5,
-            isrc : i5
-        },
-    ]
-    const handleMobileSection=()=>{
-        scrlF.current.scrollIntoView({ behavior: 'smooth' })
+  const homeImage = [
+    {
+      id: 1,
+      isrc: i1,
+    },
+    {
+      id: 2,
+      isrc: i2,
+    },
+    {
+      id: 3,
+      isrc: i3,
+    },
+    {
+      id: 4,
+      isrc: i4,
+    },
+    {
+      id: 5,
+      isrc: i5,
+    },
+  ];
+  const handleMobileSection = () => {
+    scrlF.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const handleDesktopSection = () => {
+    destopScrlF.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const handleLaptopSection = () => {
+    slidescrlF.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const changeImage = (direction) => {
+    if (direction === "right") {
+      setImageToRender(backImgControllerRight);
+      setBackImgControllerRight(backImgControllerRight + 1);
+    }
 
+    if (direction === "left") {
+      setImageToRender(backImgControllerLeft);
+      setBackImgControllerLeft(backImgControllerLeft - 1);
     }
-    const handleDesktopSection=()=>{
-        destopScrlF.current.scrollIntoView({ behavior: 'smooth' })
-    }
-    const handleLaptopSection=()=>{
-        slidescrlF.current.scrollIntoView({ behavior: 'smooth' })
-    }
-    const changeImage = (direction)=>{
-        if(direction === 'right'){
-            setImageToRender(backImgControllerRight)
-            setBackImgControllerRight(backImgControllerRight + 1)
+  };
+  useEffect(() => {
+    const slider = () => {
+      if (backImgControllerRight > 4) {
+        setBackImgControllerRight(0);
+      }
+      if (backImgControllerLeft < 0) {
+        setBackImgControllerLeft(4);
+      }
+    };
+    slider();
+  }, [backImgControllerRight, backImgControllerLeft]);
+
+  useEffect(() => {
+    let timer = 0;
+    const autoSlide = () => {
+      const intervalId = setInterval(() => {
+        if (timer > 4) timer = 0;
+        else {
+          setImageToRender(timer);
+          timer = timer + 1;
         }
-            
-        if(direction === 'left'){
-            setImageToRender(backImgControllerLeft)
-            setBackImgControllerLeft(backImgControllerLeft - 1)
-            
-        }
-    }
-    useEffect(()=>{
-        const slider = ()=>{
-            if(backImgControllerRight> 4 ){
-                setBackImgControllerRight(0)
-            }
-            if(backImgControllerLeft < 0){
-                setBackImgControllerLeft(4)
-            }
-        }
-    slider()
-    },[backImgControllerRight, backImgControllerLeft])
-           
-    useEffect(()=>{
-        let timer = 0
-        const autoSlide = ()=>{
-            const intervalId = setInterval(()=>{
-                if(timer>4)
-                timer = 0
-                else{
-                    setImageToRender(timer)
-                    timer = timer +1
-                }        
-            }, 3000)
-            return ()=> clearInterval(intervalId)
-        } 
-    autoSlide()
-    },[])
+      }, 3000);
+      return () => clearInterval(intervalId);
+    };
+    autoSlide();
+  }, []);
 
   return (
     <div className="landingpage">
-    <div className="backgroundImage_container" >
+      <div className="backgroundImage_container">
         <div className="imageSlider">
-            <img
-            className="home_image"  ref={imageRef}
+          <img
+            className="home_image"
+            ref={imageRef}
             src={homeImage[imageToRender].isrc}
             alt=""
-            />
+          />
         </div>
         <div className="Container">
-            <div className="slider">
-                <div className="leftSlider" onClick={()=>changeImage("left")}>
-                    <div className="leftSliderin">
-                        <AiOutlineLeft className='arrows'/>
-                    </div>
-                </div>
-                <div className="rightSlider"  onClick={()=>changeImage("right")}>
-                    <div className="rightSliderin">
-                        <AiOutlineRight className='arrows'/>
-                    </div>
-                </div>
+          <div className="slider">
+            <div className="leftSlider" onClick={() => changeImage("left")}>
+              <div className="leftSliderin">
+                <AiOutlineLeft className="arrows" />
+              </div>
             </div>
-            
-            <div className="firstItemContainer">
-               <div className="langingpageItems">
-                    <div className="landingpageCatagory" onClick={handleLaptopSection}>
-                        <h2>Browse Laptops</h2>
-                        <img src={laptops} alt=''></img>
-                    </div>
-                    <div className="landingpageCatagory" onClick={handleDesktopSection}>
-                        <h2>Browse Desktops</h2>
-                        <img src={computers} alt=''></img>
-                    </div>
-                    <div className="landingpageCatagory" onClick={handleMobileSection}>
-                        <h2>Browse Phones</h2>
-                        <img src={phone} alt=''></img>
-                    </div>
-               </div>
+            <div className="rightSlider" onClick={() => changeImage("right")}>
+              <div className="rightSliderin">
+                <AiOutlineRight className="arrows" />
+              </div>
             </div>
-        </div>
+          </div>
 
+          <div className="firstItemContainer">
+            <div className="langingpageItems">
+              <div
+                className="landingpageCatagory"
+                onClick={handleLaptopSection}
+              >
+                <h2>Browse Laptops</h2>
+                <img src={laptops} alt=""></img>
+              </div>
+              <div
+                className="landingpageCatagory"
+                onClick={handleDesktopSection}
+              >
+                <h2>Browse Desktops</h2>
+                <img src={computers} alt=""></img>
+              </div>
+              <div
+                className="landingpageCatagory"
+                onClick={handleMobileSection}
+              >
+                <h2>Browse Phones</h2>
+                <img src={phone} alt=""></img>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Landingpage
+export default Landingpage;
